46c8a659cfba656d9641d95e3c5c5035c877666a
Parmeet Brar <parmeetbrar216@gmail.com>
2023-12-03 15:44:56 -0800

    Second commit
    Changed from I2C to SPI. Tested code with Raspberry Pi. Works good. Added conditions for low air quality.

--- air_quality.py
+++ air_quality.py
@@ -7,36 +7,33 @@
 # Purpose: To measure air quality using Adafruit Industries 3199 sensor       
 # Description: This script reads the analog voltage off of the output pin of the sensor and converts it to ppm 
 #              values for each gas. It then prints the ppm values for each gas to the console.
-# Date last edited: 
+# Date last edited: 2023-12-03
 
 #######################################################################################################################
 
 # Imports
 import time
-import board
-import busio
-import adafruit_mics5524
+from gpiozero import MCP3008
 
-# Functions 
+# Functions
+def get_air_quality():
+    '''Purpose: Get the air quality sensor reading.
+        Return: air quality (float)
+    '''
+    input_channel = 0
+    air_quality_sensor = MCP3008(channel=input_channel)
+    
+    if air_quality_sensor.value > 0.5:
+        print("Warning: Low air quality!")
+    return "{:.4f}".format(air_quality_sensor.value)
 
-# Preprocess Definitions
+def main():
+    while True:
+        reading = get_air_quality()
+        print("Sensor reading:", reading)
+        time.sleep(2)
 
-# Classes
-
-# Code
-
-# Create library object using our Bus I2C port
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_mics5524.MICS5524(i2c)
-
-while True:
-    print("CO: ", sensor.CO)
-    print("Ammonia: ", sensor.nh3)
-    print("Ethanol: ", sensor.c2h5oh)
-    print("H2: ", sensor.h2)
-    print("Methane / Propane / Iso-Butane: ", sensor.c3h8)
-
-    time.sleep(1)
-
+if __name__ == "__main__":
+    main()
 
 ################## End of Code ########################################################################################
