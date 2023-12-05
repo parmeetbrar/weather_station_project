bd40eede9f0e13d877d889ae06a17a61c35b5186
HO WUN NG <hwng@student.ubc.ca>
2023-12-04 16:46:02 -0800

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -7,22 +7,24 @@
 # Purpose:          Create the general Sensor class
 # Description:      Create a Sensor class that stores the name of the sensor. This class should be the base class for all
 #                   sensors and the method should be overrode.
-# Date last edited: 4 Dec 2023
+# Date last edited: 2023/12/4
 
 #######################################################################################################################
 
 # Classes
 class Sensor:
-	'''
-	Purpose:    Create a base class for all type of sensor
-	'''
-	# Constructor
+	'''Create a base class for all type of sensor.'''
 	def __init__(self,name):
+		'''
+		Constructor method for the Sensor Class
+		Args: Name of the sensor (str)
+		'''
 		# Sensor name
 		self.name = name
 	
 	# Read sensor data
-	def read_sensor_data(self):
+	def read_sensor_data():
+		'''Create a method for sensor subclass to override, raise if this method is called here.'''
 		raise Exception("The read_sensor_data method should be overridden by subclasses.")
 
 ################## End of Code ########################################################################################
